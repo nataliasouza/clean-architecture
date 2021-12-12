ddc7e3b3bfae8fee30e0822456df5cc238e52be4
Natalia Souza <nataliasouza.ti@hotmail.com>
2021-12-12 18:35:14 -0300

    Created Base Project and entities: Category with validation and Product

---
+++ CleanArchDomain/Entities/Category.cs
@@ -0,0 +1,34 @@
+﻿using CleanArch.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArch.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name.Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3 ,"Invalid name, too short, minimum 3 charecters is required");
+
+            Name = name;
+        }
+    }
+}
