a7e8f45bdf62c1ff82087c0c96a89ad76ef199e2
Natalia Souza <nataliasouza.ti@hotmail.com>
2021-12-13 22:34:05 -0300

    Added Context and EntitiesConfiguration

---
+++ src/CleanArchInfra/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categoriers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+    }
+}
