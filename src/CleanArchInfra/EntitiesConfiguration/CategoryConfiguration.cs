315e966c57478459fe8dd3878600ee1a6eecf546
Natalia Souza <nataliasouza.ti@hotmail.com>
2021-12-14 22:18:41 -0300

    adjustment Id category

--- src/CleanArchInfra/EntitiesConfiguration/CategoryConfiguration.cs
+++ src/CleanArchInfra/EntitiesConfiguration/CategoryConfiguration.cs
@@ -13,8 +13,8 @@ namespace CleanArch.Infra.EntitiesConfiguration
 
             builder.HasData(
                 new Category(1, "Material Escolar"),
-                new Category(1, "Eletrônicos"),
-                new Category(1, "Acessórios")
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
                 );
         }
     }
